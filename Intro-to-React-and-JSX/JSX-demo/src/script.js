430bf2950fa1742dbbd0cad323dc66260ac1b47a
Rangel Shishkov <93160824+RangelShishkov@users.noreply.github.com>
2024-05-21 00:12:16 +0300

    intro-JSX-REACT

---
+++ Intro-to-React-and-JSX/JSX-demo/src/script.js
@@ -0,0 +1,31 @@
+import "../node_modules/react/umd/react.production.min.js";
+import "../node_modules/react-dom/umd/react-dom.production.min.js";
+
+const rootDomElement = document.getElementById("root");
+
+const root = ReactDOM.createRoot(rootDomElement);
+
+// const reactHeading = React.createElement("h1", {}, "Hello from JSX!");
+// const reactSecondHeading = React.createElement(
+//   "h2",
+//   {},
+//   "The best syntax ever!"
+// );
+// const header = React.createElement(
+//   "header",
+//   { className: "site-header" },
+//   reactHeading,
+//   reactSecondHeading
+// );
+
+const headerJSX = (
+  <header className="site-header">
+    <h1>Hello from JSX!</h1>
+    <h2>The best syntax ever!</h2>
+
+    <p>Something here!</p>
+    <p>Another thing here!</p>
+  </header>
+);
+
+root.render(headerJSX);
---
+++ Intro-to-React-and-JSX/vite-demo/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>My first react application!</h1>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
